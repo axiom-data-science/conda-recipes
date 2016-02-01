bac9ed8f7bc84d5632677e5ee033c6b6f63e66b8
Kyle Wilcox <kyle@axiomalaska.com>
2016-02-01 16:51:15 -0500

    bump billiard build and use py27 to build it

--- billiard/release.sh
+++ billiard/release.sh
@@ -9,15 +9,18 @@ if [ $# -eq 0 ]; then
 fi
 
 # Build 2.7
+sa build27
 conda build --python 2.7 .
 PACKAGE_PATH=`conda build --python 2.7 --output .`
-conda convert --platform all $PACKAGE_PATH -o ./py27
+conda convert --platform linux-32 $PACKAGE_PATH -f -o ./py27
+conda convert --platform linux-64 $PACKAGE_PATH -f -o ./py27
 for f in ./py27/**/$NAME*; do
     anaconda upload -u $ORG --force $f
 done
 rm -r ./py27
 
 # Build 3.4
+sd
 conda build --python 3.4 .
 PACKAGE_PATH=`conda build --python 3.4 --output .`
 conda convert --platform all $PACKAGE_PATH -o ./py34
